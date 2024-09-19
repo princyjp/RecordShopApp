f3338839bbc18e2e20f8a78c5ce0bb7c0308f8a6
princyjp <princy.philip@outlook.com>
2024-09-19 14:44:02 +0100

    feat: add post method in albumApiService

--- app/src/main/java/com/example/recordshopapp/service/AlbumApiService.java
+++ app/src/main/java/com/example/recordshopapp/service/AlbumApiService.java
@@ -1,14 +1,20 @@
 package com.example.recordshopapp.service;
 
 import com.example.recordshopapp.model.Album;
+import com.example.recordshopapp.model.AlbumDTO;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
 
     @GET("albums")
     Call<List<Album>> getAllAlbums();
+
+    @POST("albums")
+    Call<AlbumDTO> postAlbum(@Body AlbumDTO albumDTO);
 }
