98a3617273e291db8370bac9228ff90d7525dfee
princyjp <princy.philip@outlook.com>
2024-09-19 16:56:48 +0100

    feat: implement data binding in addNewAlbummActivity class

--- app/src/main/java/com/example/recordshopapp/ui/MainActivity/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/example/recordshopapp/ui/MainActivity/addalbum/AddNewAlbumActivity.java
@@ -7,11 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.recordshopapp.R;
+import com.example.recordshopapp.databinding.ActivityAddNewAlbumBinding;
+import com.example.recordshopapp.model.Album;
+import com.example.recordshopapp.model.AlbumDTO;
+import com.example.recordshopapp.ui.MainActivity.AlbumAdapter;
+import com.example.recordshopapp.ui.MainActivity.MainActivityViewModel;
+
+import java.util.List;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
-
+    private AddAlbumClickHandlers addAlbumClickHandlers;
+    private MainActivityViewModel mainActivityViewModel;
+    private AlbumDTO albumDTO;
+    private ActivityAddNewAlbumBinding activityAddNewAlbumBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +36,11 @@ public class AddNewAlbumActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        activityAddNewAlbumBinding = DataBindingUtil.setContentView(this,R.layout.activity_add_new_album);
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        activityAddNewAlbumBinding.setAlbumDTO(albumDTO);
+        activityAddNewAlbumBinding.setClickHandler(addAlbumClickHandlers);
+
     }
+
 }
