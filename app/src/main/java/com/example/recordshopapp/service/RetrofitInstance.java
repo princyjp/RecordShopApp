2515a682c1803c994831f8ddf7661dd11cb69043
princyjp <princy.philip@outlook.com>
2024-09-19 11:57:05 +0100

    feat: create retrofitInstance class

---
+++ app/src/main/java/com/example/recordshopapp/service/RetrofitInstance.java
@@ -0,0 +1,26 @@
+package com.example.recordshopapp.service;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "http://192.168.28.234:8080/api/v1/recordshop/";
+
+    public static AlbumApiService getService(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).connectTimeout(30, TimeUnit.SECONDS) // Increase connection timeout
+                .readTimeout(30, TimeUnit.SECONDS)    // Increase read timeout
+                .writeTimeout(30, TimeUnit.SECONDS).build();
+
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).client(client).build();
+        }
+        return retrofit.create(AlbumApiService.class);
+    }
+}
