9b1d17160753a27095eda6dfe49e522d1bc75534
princyjp <princy.philip@outlook.com>
2024-09-23 10:11:59 +0100

    feat: add PUT and DELETE to Al AlbumApiService class

--- app/src/main/java/com/example/recordshopapp/service/AlbumApiService.java
+++ app/src/main/java/com/example/recordshopapp/service/AlbumApiService.java
@@ -7,8 +7,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface AlbumApiService {
 
@@ -17,4 +20,10 @@ public interface AlbumApiService {
 
     @POST("albums")
     Call<AlbumDTO> postAlbum(@Body AlbumDTO albumDTO);
+
+    @PUT("albums/{id}")
+    Call<AlbumDTO> updateAlbum(@Path("id") long id, @Body AlbumDTO albumDTO);
+
+    @DELETE("albums/{id}")
+    Call<String> deleteAlbum(@Path("id") long id);
 }
